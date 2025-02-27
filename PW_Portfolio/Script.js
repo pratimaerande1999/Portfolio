282ff64e27da17348664521a7b74906e10c469ab
pratimaerande1999 <91843804+pratimaerande1999@users.noreply.github.com>
2025-02-27 11:09:00 +0100

    Add files via upload

---
+++ PW_Portfolio/Script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".auto-type",{
+    strings : ["Web Developer","Frontend Developer","Backend Developer","SQL Developer","Fullstack Developer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
